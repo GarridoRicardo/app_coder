c91755ce40d933d25043a85399bbc58550148acb
Ricardo Garrido <ricardoa.garridop@gmail.com>
2021-11-07 19:17:46 -0300

    ajuste ItemDetailContainer

--- src/ItemDetailContainer.js
+++ src/ItemDetailContainer.js
@@ -11,19 +11,21 @@ const ItemDetailContainer = () => {
     const promesa = new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(productos.find(p => p.id == id))
-        }, 2000)
+        }, 1000)
     });
     
     promesa.then(producto => setProducto(producto))
         }, [id])
 
+    if(producto.length === 0){
+            return <div>Cargando Productos, por favor espere...</div>
+    }else{
     return (
         <div>
             <ItemDetail item={productos}/>
         </div>
-        
-
     )
+}
 }
 
 export default ItemDetailContainer
