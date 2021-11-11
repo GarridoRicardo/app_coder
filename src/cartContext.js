ecc5c1cced642ca0a16b4eec4baa6ee8631200b1
Ricardo Garrido <ricardoa.garridop@gmail.com>
2021-11-10 22:02:28 -0300

    _

--- src/cartContext.js
+++ src/cartContext.js
@@ -6,14 +6,40 @@ const {Provider} = contexto;
 const CustomComponet = ({children}) => {
    
     const [cart, setCart] = useState([]);
+
+/*     const agregarProducto = (producto) => {
+            const nuevoProducto = {cantidad,producto};
+            const copia = [...cart];
+            copia.push(nuevoProducto);
+            setCart(copia);
+    }
+
+    const eliminarProducto = (producto) => {
+        const copia = [...cart];
+        const index = copia.findIndex(item => item.producto === producto);
+        copia.splice(index,1);
+        setCart(copia);}
+    
+        const isInCart = (producto) => {
+            const index = cart.findIndex(item => item.producto === producto);
+            return index >= 0;
+        }
+            const vaciar = () => {
+        setCart([]);} */
+    
+
     const addToCart = (producto,cantidad) => {
         console.log("soy el contexto en cart");
         console.log(producto,cantidad);
-    }    
+    }
+
     const valorDelContexto = {
         cart: cart,
         addToCart: addToCart
     }
+
+
+
     return (
         <Provider value={valorDelContexto}>
             {children}
