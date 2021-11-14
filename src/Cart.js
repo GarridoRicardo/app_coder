cf49a7a1eb5f7fbedf5aba2bfdc2e3a20f059e60
Ricardo Garrido <ricardoa.garridop@gmail.com>
2021-11-14 15:13:10 -0300

    optimizando contexto

--- src/Cart.js
+++ src/Cart.js
@@ -2,13 +2,26 @@ import { useContext } from "react";
 import { contexto } from "./cartContext";
 
 const Cart = () => {
+
     const resultado = useContext(contexto);
-    console.log(resultado);
     return (
-        <div>
-            <p>Aca esta tu compra</p>
-        </div>
+        <main>
+            {resultado.length == 0?<p>Tu carrito esta vacio</p>:
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Producto</th>
+                            <th>Nombre del producto</th>
+                            <th>Cantidad</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    
+                </table>
+            }
+        </main>
     )
+
 }
 
 export default Cart
--- src/cartContext.js
+++ src/cartContext.js
@@ -1,5 +1,4 @@
 import {createContext , useState} from 'react';
-
 export const contexto = createContext();
 
 const {Provider} = contexto;
@@ -7,16 +6,36 @@ const CustomComponet = ({children}) => {
 
     const [cart, setCart] = useState([]);
 
+    //Agregando Productos al carrito
     const addToCart = (producto,cantidad) => {
-        console.log("soy agregar producto");
-        console.log(producto,cantidad);
+        //Aplico condicional para verificar si el producto ya existe en el carrito
+        if(isInCart(producto.id)){
+            //Ejecuto un map para recorrer el la copia de cart. 
+            //Si el producto ya existe en el carrito es decir que su id es igual, entonces "pusheo" la cantidad al item, sino solo dejo el item
+            setCart([...cart.map(item => {
+                if(item.producto.id === producto.id){
+                    item.cantidad += cantidad;
+                }
+                return item;
+            })])
+
+        //Si es distinto entonces pusheo el producto al carrito
+        }else{
         setCart([...cart, {producto,cantidad}]);
+    }}
+
+    //Funcion que me valida si el producto ya esta en el carrito
+    const isInCart = (id) => {
+        const productoCarrito = cart.find(item => item.producto.id === id);
+        return productoCarrito;
     }
 
+    //Restando Productos del carrito
     const removeFromCart = (producto) => {
         setCart([...cart.filter(item => item.producto.id !== producto.id)]);
     }
 
+     //Vaciar carrito
     const vaciar = () => {
         setCart([]);
     }
@@ -34,10 +53,3 @@ const CustomComponet = ({children}) => {
         </Provider>)
 }
 export default CustomComponet
-
-/*     
-        const isInCart = (producto) => {
-            const index = cart.findIndex(item => item.producto === producto);
-            return index >= 0;
-        }
-    */
